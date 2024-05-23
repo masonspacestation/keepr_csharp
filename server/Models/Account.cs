d54c95ec84777843724c26e21783b3b42fdf4446
masonspacestation <71409121+masonspacestation@users.noreply.github.com>
2024-05-23 15:00:37 -0600

    init

---
+++ server/Models/Account.cs
@@ -0,0 +1,6 @@
+namespace keepr_csharp.Models;
+
+public class Account : Profile
+{
+  public string Email { get; set; }
+}
---
+++ server/Models/RepoItem.cs
@@ -0,0 +1,8 @@
+namespace keepr_csharp.Models;
+
+public class RepoItem<T>
+{
+  public T Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+}
